75dfa35dd929192d628cb06a830aca26f8c3dcdf
ZhongXi <xiezhongxi0525@foxmail.com>
2016-01-16 23:30:35 +0800

    first commit
    creat universal version framework

---
+++ Products/UniversalFramework.framework/Headers/UniversalFramework.h
@@ -0,0 +1,13 @@
+//
+//  UniversalFramework.h
+//  UniversalFramework
+//
+//  Created by FreeGeek on 16/1/12.
+//  Copyright © 2016年 ZhongXi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UniversalFramework : NSObject
++(void)getMessage;
+@end
---
+++ UniversalFramework/UniversalFramework.m
@@ -0,0 +1,16 @@
+//
+//  UniversalFramework.m
+//  UniversalFramework
+//
+//  Created by FreeGeek on 16/1/12.
+//  Copyright © 2016年 ZhongXi. All rights reserved.
+//
+
+#import "UniversalFramework.h"
+
+@implementation UniversalFramework
++(void)getMessage
+{
+    NSLog(@"get Message Success");
+}
+@end
